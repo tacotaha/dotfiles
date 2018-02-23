d3d7d7e4c85952770f6f350a3566b0e1c65d69e4
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-02-22 20:53:04 -0500

    Added Octave-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -203,3 +203,17 @@
 ;;(load-theme 'atom-one-dark t)
 ;;(load-theme 'word-perfect t t)
 ;;(enable-theme 'word-perfect)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ac-octave rust-mode yasnippet which-key use-package try smartparens org-bullets ggtags flycheck counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
