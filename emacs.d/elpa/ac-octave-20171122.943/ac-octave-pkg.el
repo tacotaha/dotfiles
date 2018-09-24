245f01cd2b24cb81a931ac7e490b1639cb5b4b9c
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-23 23:30:00 -0400

    Goodbye emacs

--- emacs.d/elpa/ac-octave-20171122.943/ac-octave-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ac-octave" "20171122.943" "An auto-complete source for Octave" '((auto-complete "1.4.0")) :commit "6d09b94a86f43de84c60e9a699b5e1be61c0f138" :url "https://github.com/coldnew/ac-octave" :keywords '("octave" "auto-complete" "completion"))
--- emacs.d/elpa/swiper-20180124.1142/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20180124.1142" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "e8662ce487bba78827e735d7d4ff70c579a62191" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
